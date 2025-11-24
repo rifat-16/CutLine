aed7e93747bc772a02ad3444ba3e4b6bf8f769eb
rifat <rifat6teen@gmail.com>
2025-11-24 14:41:08 +0600

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
-import 'package:cutline/barber/screens/barber_home_screen.dart';
-import 'package:cutline/owner/screens/owner_home_screen.dart';
-import 'package:cutline/ui/screens/login_screen.dart';
-import 'package:cutline/ui/screens/role_selection_screen.dart';
-import 'package:cutline/ui/screens/signup_screen.dart';
-import 'package:cutline/ui/screens/splash_screen.dart';
-import 'package:cutline/ui/screens/user/salon_details_screen.dart';
-import 'package:cutline/ui/screens/user/user_home_screen.dart';
+import 'package:cutline/features/owner/screens/owner_home_screen.dart';
+import 'package:cutline/features/auth/screens/login_screen.dart';
+import 'package:cutline/features/auth/screens/role_selection_screen.dart';
+import 'package:cutline/features/auth/screens/signup_screen.dart';
+import 'package:cutline/features/user/screens/salon_details_screen.dart';
+import 'package:cutline/features/user/screens/user_home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const CutLineApp());
@@ -17,21 +16,25 @@ class CutLineApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'CutLine',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const BarberHomeScreen(),
-        '/welcome': (context) => const RoleSelectionScreen(),
-        '/login': (context) => const LoginScreen(),
-        '/signup': (context) => const SignupScreen(),
-        '/user-home': (context) => const UserHomeScreen(),
-        '/owner-home': (context) => const OwnerHomeScreen(),
-        '/salon-details': (context) => SalonDetailsScreen(
-              salonName: ModalRoute.of(context)?.settings.arguments as String? ?? '',
-            ),
-      },
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      minTextAdapt: true,
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'CutLine',
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const OwnerHomeScreen(),
+          '/welcome': (context) => const RoleSelectionScreen(),
+          '/login': (context) => const LoginScreen(),
+          '/signup': (context) => const SignupScreen(),
+          '/user-home': (context) => const UserHomeScreen(),
+          '/owner-home': (context) => const OwnerHomeScreen(),
+          '/salon-details': (context) => SalonDetailsScreen(
+                salonName: ModalRoute.of(context)?.settings.arguments as String? ?? '',
+              ),
+        },
+      ),
     );
   }
 }
